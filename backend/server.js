84a59e43431f728fde41d7ff7c5378aba9b06aa4
Ewerton Luiz <ewerton.luiz_1994@hotmail.com>
2025-02-17 22:14:55 -0300

    agora vai de verdade04

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
