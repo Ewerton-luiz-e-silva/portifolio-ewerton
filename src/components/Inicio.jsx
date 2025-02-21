1b7e7cfdea2e2624d26510bbf7fe9b10cac97bf1
Ewerton Luiz <ewerton.luiz_1994@hotmail.com>
2025-02-21 11:21:39 -0300

    ajustando style e-mail-07

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./css/inicio.css"; // Importe o CSS correspondente
 import "./css/style.css";
 
+import CorriculoEwerton from '/doc/CorriculoEwerton.docx';
 
 const Inicio = () => {
 return (
@@ -36,7 +37,7 @@ return (
             </svg>
         </a>
         <a
-            href="./doc/Modelo  Corriculo Ewerton.docx"
+            href="/CorriculoEwerton.docx"
             className="cta curriculo"
             download
             data-aos="fade-up"
