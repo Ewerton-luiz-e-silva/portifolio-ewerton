081472dc7d53dd57439599e249922c46be25dece
Ewerton Luiz <ewerton.luiz_1994@hotmail.com>
2025-03-02 18:34:35 -0300

    atualizando style so portfolio

--- src/components/Servicos.jsx
+++ src/components/Servicos.jsx
@@ -9,7 +9,7 @@ import gifServicos from './img/Gif serviços.gif';
 const Servicos = () => {
 useEffect(() => {
     AOS.init({
-    duration: 300, // Duração das animações
+    duration: 500, // Duração das animações
     once: true,     // Animação ocorre apenas uma vez
     });
 }, []);
